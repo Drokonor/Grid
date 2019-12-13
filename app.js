d988d0fd9e1e0212ae97802a64959e75273efae4
Drokonor <terminus_drok@gmail.com>
2019-12-13 18:56:03 +0300

    Buttons
    Buttons have added

--- app.js
+++ app.js
@@ -2,8 +2,22 @@ const express = require('express')
 
 const app = express()
 
+app.set("view engine", "ejs")
+
 app.get('/', function(req, res){
-    res.send('Hello world')
+    res.render('index')
 })
 
-app.listen(8080)
+app.post('/create', function(req,res){
+    console.log("create")
+    res.redirect('/')
+})
+
+app.post('/remove', function(req,res){
+    console.log("remove")
+    res.redirect('/')
+})
+
+app.listen(8080, function(){
+    console.log('Сервер запущен на порте 8080')
+})
